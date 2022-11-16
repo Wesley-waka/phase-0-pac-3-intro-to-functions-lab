a9467cc90e9ceae3dfb0edd88110cee653ffd2c7
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-16 18:49:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,56 @@
+const string=`hello`;
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  
+  function logShout (hello) {
+    console.log(hello.toUpperCase( ));
+  }
+
+  function logWhisper (hello) {
+    console.log(hello.toLowerCase());
+  }
+
+ function sayHiToHeadphonedRoommate(string){
+    if(string===string.toLowerCase()){
+        return `I can't hear you!`;
+
+    }else if(string===string.toUpperCase()){
+        return `YES INDEED!`;
+
+    }else if(string==="Let's have dinner together!"){
+        return `I would love to!`;
+    }
+}
+
+
+
+
+//     return `I can't hear you!`;
+  
+ 
+//  if (sayHiToHeadphonedRoommate=HELLO) {
+//     `"YES INDEED!"`;
+//   } 
+//   if (sayHiToHeadphonedRoommate="Let's have dinner together!")
+//   {
+//     "I would love to!";
+//   }
+
+
+
+
+
+ 
+
+
+ 
+
+  
+
+ 
+ 
